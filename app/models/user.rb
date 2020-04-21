fd87e839b49844d6c0b6e269c9ad082a9f463b8b
na-toyoshima <na.toyoshim@gmail.com>
2020-04-21 17:13:53 +0900

    following 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true, length: { minimum: 2, maximum: 20 }
-         validates :introduction, length: { maximum: 50 }
-         has_many :books, dependent: :destroy
-         attachment :profile_image
+        validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+        validates :introduction, length: { maximum: 50 }
+        has_many :books, dependent: :destroy
+        attachment :profile_image
+
+        has_many :relationships
+        has_many :followings, through: :relationships, source: :follow
+        has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+        has_many :followers, through: :reverse_of_relationships, source: :user
+
 end
